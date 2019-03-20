4e73a04360a7db6a1f34cc73439b7815b2c9338b
Broitman <gabrielbroitmanpinheiro@gmail.com>
2019-03-19 23:25:41 -0300

    Ajuste libs

--- weedem/src/weedem/utils/conexao/FabricaConexao.java
+++ weedem/src/weedem/utils/conexao/FabricaConexao.java
@@ -6,36 +6,36 @@ import java.sql.DriverManager;
 public class FabricaConexao {
 
 	private Connection conexao;
-	
+	final private String host = "xxxxxxxxxxxxxxxxxxxxxxxx";
+	final private String user = "xxxxxxxx";
+	final private String passwd = "xxxxxxxxxxx";
+
 	public Connection fazerConexao() {
-		
+
 		try {
-			
-			Class.forName("org.postgresql.Driver");
-			
-			this.conexao = DriverManager.getConnection(
-					"jdbc:postgresql://localhost:5432/web_cam_20182, user, password)",
-					"postgres",
-					"postgrelasalle"
-					);
-			
+
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Setup the connection with the DB
+			this.conexao = DriverManager
+					.getConnection("jdbc:mysql://" + host + "/feedback?" + "user=" + user + "&password=" + passwd);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return this.conexao;
 	}
-	
+
 	public void fecharConexao() {
-		
+
 		try {
-			
+
 			this.conexao.close();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-}
 
+}
