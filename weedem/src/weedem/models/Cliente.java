d114d5620be630b0fed765946ae1d4ed89a1b66b
Broitman <gabrielbroitmanpinheiro@gmail.com>
2019-03-21 03:13:48 -0300

    fudeu

--- weedem/src/weedem/models/Cliente.java
+++ weedem/src/weedem/models/Cliente.java
@@ -1,9 +1,5 @@
 package weedem.models;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-
-@ManagedBean @RequestScoped
 public class Cliente extends Pessoa {
 
 	private static final long serialVersionUID = 1L;
