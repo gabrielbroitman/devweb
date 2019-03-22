281a697ee0f8f69e70560b7b290184a6b3a7b968
Broitman <gabrielbroitmanpinheiro@gmail.com>
2019-03-22 03:19:47 -0300

    Funcionando!

---
+++ weedem/src/weedem/usuario/servico/ClienteBean.java
@@ -0,0 +1,31 @@
+package weedem.usuario.servico;
+
+import javax.faces.bean.ManagedBean;
+
+import weedem.models.Cliente;
+
+@ManagedBean
+public class ClienteBean {
+	
+	private Cliente cliente = new Cliente();
+	
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	private String nome;
+	
+	public void gravar() {
+		System.out.println("Salvando cliente: " + this.cliente.getNome() + " !");
+	}
+
+}
