188dfb97e503076124aed7b0af53b5e472ea7329
Gabriel Broitman Pinheiro <gabriel.broitman@bndes.gov.br>
2019-05-28 15:15:24 -0300

    ajuste

--- weedem/src/weedem/usuario/controller/ClienteCadastroBean.java
+++ weedem/src/weedem/usuario/controller/ClienteCadastroBean.java
@@ -15,6 +15,7 @@ public class ClienteCadastroBean {
 	private String nome;
 	private String endereco;
 	private String email;
+	private ClienteDAO clienteDAO = new ClienteDAO();
 
 	private Cliente cliente = new Cliente();
 
@@ -59,43 +60,34 @@ public class ClienteCadastroBean {
 	}
 
 	public void gravar() throws SQLException {
-		System.out.println("Salvando cliente: " + this.cliente.getNome() + " !");
+		System.out.println("Salvando cliente: " + this.cliente.getNome());
+
 		if (this.cliente == null) {
 			throw new RuntimeException("Cliente deve existir!");
 		}
+		try {
+			if (this.clienteDAO.buscarPorId(this.getId()) != null) {
+				throw new RuntimeException("Cliente com esse id já existe.");
+			}
 
-		new ClienteDAO().inserir(this.cliente);
-	}
+			this.clienteDAO.inserir(this.cliente);
+			System.out.println("Cliente salvo" + this.cliente.getNome() + "com sucesso!");
 
-	public void atualizar() throws SQLException {
-		System.out.println("Atualizando cliente: " + this.cliente.getNome() + " !");
-		if (this.cliente == null) {
-			throw new RuntimeException("Cliente deve existir!");
+		} catch (Exception e) {
+			throw e;
 		}
-
-		new ClienteDAO().atualizar(this.cliente);
 	}
-	
-	public void buscarClientePorId() throws SQLException {
-		System.out.println("Buscando cliente de id: " + this.cliente.getId() + " !");
-		if (this.cliente == null) {
-			throw new RuntimeException("Cliente deve existir!");
-		}
 
-		this.cliente = new ClienteDAO().buscarPorId(this.getId());
-		
+	public String getNome() {
+		return nome;
 	}
-	
-	public void excluirClientePorId() throws SQLException {
-		System.out.println("Excluindo cliente de id: " + getId() + " !");
-		if (this.cliente == null) {
-			throw new RuntimeException("Cliente deve existir!");
-		}
 
-		new ClienteDAO().excluir(this.getId());
+	public String getEndereco() {
+		return endereco;
 	}
-	
-	
-	
-	
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
 }
--- weedem/src/weedem/usuario/controller/ClienteDetalheBean.java
+++ weedem/src/weedem/usuario/controller/ClienteDetalheBean.java
@@ -15,34 +15,75 @@ public class ClienteDetalheBean {
 	private String nome;
 	private String endereco;
 	private String email;
+	private boolean pesquisado = false;
+	private boolean excluido = false; 
+	private boolean falha = false;
 
 	private Cliente cliente = new Cliente();
+	private ClienteDAO clienteDAO = new ClienteDAO();
+	
+	public boolean isFalha() {
+		return falha;
+	}
+
+	public void setFalha(boolean falha) {
+		this.falha = falha;
+	}
+
+	public boolean getPesquisado() {
+		return pesquisado;
+	}
+
 
 	public ClienteDetalheBean() {
 
 	}
-
+	
 	public Cliente getCliente() {
 		return cliente;
 	}
 	
+	
 	public void buscarClientePorId() throws SQLException {
-		System.out.println("Buscando cliente de id: " + this.cliente.getId() + " !");
-		if (this.cliente == null) {
-			throw new RuntimeException("Cliente deve existir!");
+		this.excluido = false;
+		System.out.println("Buscando cliente de id: " + this.id + " !");
+		
+		//this.cliente = this.clienteDAO.buscarPorId(this.getId());
+
+		
+		this.cliente.setId(1);
+		this.cliente.setNome("Carlo");
+		
+		System.out.println(this.cliente.getNome());
+		this.falha = false;
+		this.pesquisado = true;
+		
+		if (this.cliente == null || this.getId() == 0) {
+			this.pesquisado = false;
+			this.falha = true;
 		}
-		
-		this.cliente = new ClienteDAO().buscarPorId(this.getId());
-		
 	}
 	
 	public void excluirClientePorId() throws SQLException {
-		System.out.println("Excluindo cliente de id: " + getId() + " !");
+		System.out.println("Excluindo cliente de id: " + this.id + " !");
 		if (this.cliente == null) {
 			throw new RuntimeException("Cliente deve existir!");
 		}
 		
-		new ClienteDAO().excluir(this.getId());
+		//this.excluido = this.clienteDAO.excluir(this.getId());
+		this.excluido = true;
+		this.cliente = null;
+		this.pesquisado = false;
+		
+		System.out.println("Exclusão do cliente " + this.cliente.getNome() + "feita? " + excluido);
+	}
+
+	public boolean isExcluido() {
+		return excluido;
+	}
+
+	public void setExcluido(boolean excluido) {
+		this.excluido = excluido;
 	}
 
 	public int getId() {
